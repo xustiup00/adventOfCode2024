a8d2a29367f87005b8e8b396bfc1ae9b41e05914
xustiup00 <xustiup00@vutbr.cz>
2024-12-25 17:17:30 +0100

    add first solution

---
+++ 1st.py
@@ -0,0 +1,26 @@
+list1 = [] #left list
+list2 = [] #right list
+
+f = open("input.txt", "r")
+
+#divide numbers into lists
+for line in f:
+  splited = line.split()
+  list1.append(int(splited[0]))
+  list2.append(int(splited[1]))
+          
+#sort lists
+sortedList1 = sorted(list1)
+sortedList2 = sorted(list2)
+    
+sum = 0  
+length = len(sortedList1)
+index = 0
+#calculate the difference and add up to the sum
+while index != length:   
+  sum += abs(sortedList1[index] - sortedList2[index])
+  index += 1
+
+print(sum)
+
+f.close()
